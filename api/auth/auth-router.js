c96cf7a285ae59bc9e9e4dc9a06197f6cf57ba01
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2022-02-22 13:49:20 -0500

    one error

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,46 +1,44 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const Users = require('../users/users-model')
+const {checkPasswordLength, checkUsernameExists, checkUsernameFree} = require('./auth-middleware')
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
 
+router.post('/register', checkUsernameFree, checkPasswordLength, (req, res, next) => {
+    const { username, password } = req.body;
+    const hash = bcrypt.hashSync(password, 12);
+    Users.add({ username, password: hash})
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(next)
+})
 
-/**
-  1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "user_id": 2,
-    "username": "sue"
+router.post('/login', checkUsernameExists, (req, res, next) => {
+  const { password } = req.body;
+  if(bcrypt.compareSync(password, req.user.password)) {
+    req.session.user = req.user;
+    res.status(200).json({ message: `Welcome ${req.user.username}`})
+  } else {
+    next({ status: 401, message: 'Invalid credentials' })
   }
-
-  response on username taken:
-  status 422
-  {
-    "message": "Username taken"
-  }
-
-  response on password three chars or less:
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
- */
+})
 
 
-/**
-  2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "message": "Welcome sue!"
-  }
-
-  response on invalid credentials:
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
- */
+router.get('/logout', (req, res, next) => {
+  if(req.session.user) {
+    req.session.destroy(err => {
+      if (err) {
+        next(err)
+      } else {
+        res.status(200).json('logged out')
+      }
+    })
+      } else {
+        next({ status: 200, message: 'no session' })
+      }
+})
 
 
 /**
@@ -61,3 +59,4 @@
 
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,19 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
+const router = require('express').Router();
+const Users = require('./users-model');
+const { restricted } = require('../auth/auth-middleware')
 
+
+router.get("/", restricted, (req, res, next) => {
+  Users.find()
+    .then(users => {
+      console.log(users)
+      res.status(200).json(users)
+    })
+    .catch(next)
+})
+
+module.exports = router
 
 /**
   [GET] /api/users
